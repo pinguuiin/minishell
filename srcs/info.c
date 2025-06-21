339be95a88095bc22e7f0057494cbae9b81592d5
Donghyun <hdh1324@gmail.com>
2025-06-21 15:07:47 +0300

    add info related functions

---
+++ srcs/info.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   info.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donheo <donheo@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/21 13:23:43 by donheo            #+#    #+#             */
+/*   Updated: 2025/06/21 13:37:44 by donheo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_info	*get_info(void)
+{
+	static t_info	info;
+
+	return (&info);
+}
+
+void	init_info(char **envp)
+{
+	t_info	*info;
+
+	info = get_info();
+	info->arena = arena_create(ARENA_BLOCK_SIZE);
+	info->is_error = 0;
+	info->error_code = 0;
+	info->envp_copy = envp_copy(envp, info);
+}
+
