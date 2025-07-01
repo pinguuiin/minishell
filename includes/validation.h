fc7c8f7fbed996403120f5279d9dfba6eee9e13e
Donghyun <hdh1324@gmail.com>
2025-07-01 05:47:22 +0300

    add const for read-only parameters

--- includes/validation.h
+++ includes/validation.h
@@ -6,7 +6,7 @@
 /*   By: donheo <donheo@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 22:18:49 by donheo            #+#    #+#             */
-/*   Updated: 2025/06/30 10:17:04 by donheo           ###   ########.fr       */
+/*   Updated: 2025/07/01 05:33:18 by donheo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 int		is_all_whitespace(const char *input);
 int		has_balanced_quotes(const char *cmd);
 int		check_syntax_error(const char *cmd);
-void	update_quote_state(char c, int *in_single_quote, int *in_double_quote);
+void	update_quote_state(const char c, \
+	int *in_single_quote, int *in_double_quote);
 
 #endif
