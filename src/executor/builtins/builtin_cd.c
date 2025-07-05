57d3e4ff1545f3b8feed728c3499928361549354
pinguuiin <yuping199407@hotmail.com>
2025-07-05 08:09:07 +0300

    Fix cd when trying to go to the parent directory but current folder is deleted

--- src/executor/builtins/builtin_cd.c
+++ src/executor/builtins/builtin_cd.c
@@ -47,14 +47,27 @@ static int	update_env_pwd(char **envp, char *oldpwd, char *pwd)
 	return (EXIT_SUCCESS);
 }
 
+static char	*expand_path(char *path, char *pwd)
+{
+	char	*ptr;
+	char	*parent_path;
+
+	if (ft_strncmp(path, "..", 3))
+		return (path);
+	ptr = ft_strrchr(pwd, '/');
+	parent_path = aalloc(&get_info()->arena, ptr - pwd + 2);
+	if (!parent_path)
+		clean_and_exit("cd");
+	ft_strlcpy(parent_path, pwd, ptr - pwd + 2);
+	return (parent_path);
+}
+
 int	cd(char **argv, char **envp)
 {
-	char	oldpwd[PATH_MAX];
-	char	pwd[PATH_MAX];
+	char	*oldpwd;
+	char	*pwd;
 	char	*path;
 
-	if (!getcwd(oldpwd, sizeof(oldpwd)))
-		return (error_msg("minishell", NULL, "cd", 1));
 	if (!argv[1])
 	{
 		path = envp[get_env_ind(envp, "HOME")];
@@ -66,9 +79,11 @@ int	cd(char **argv, char **envp)
 		return (error_msg("minishell", "cd", "too many arguments", 1));
 	else
 		path = argv[1];
-	if (chdir(path) == -1)
+	if (getpwd(&oldpwd, envp))
 		return (error_msg("minishell", NULL, "cd", 1));
-	if (!getcwd(pwd, sizeof(pwd)))
+	if (chdir(expand_path(path, oldpwd)) == -1)
+		return (error_msg("minishell", NULL, "cd", 1));
+	if (getpwd(&pwd, envp))
 		return (error_msg("minishell", NULL, "cd", 1));
 	return (update_env_pwd(envp, oldpwd, pwd));
 }
